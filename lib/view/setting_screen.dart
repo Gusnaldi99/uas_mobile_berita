abbfe42d12f0d41131f76026fedb994d2735d266
Gusnaldi99 <gusnaldi.luthfi99@gmail.com>
2025-01-10 22:10:24 +0700

    About Us

--- lib/view/setting_screen.dart
+++ lib/view/setting_screen.dart
@@ -32,67 +32,15 @@ class SettingsScreen extends StatelessWidget {
             trailing: const Icon(Icons.chevron_right),
             onTap: () {
               // navigator push berfungsi ketika klik listtile akan mengarah ke halaman about
-
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => const AboutScreen(),
-              //   ),
-              // );
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AboutScreen(),
+                ),
+              );
             },
           ),
           const Divider(),
-          // ListTile(
-          //   leading: const Icon(Icons.lock),
-          //   title: const Text('Change Password'),
-          //   trailing: const Icon(Icons.chevron_right),
-          //   onTap: () {
-          //     // Navigate to Change Password Screen
-          //   },
-          // ),
-          // const Divider(),
-          // ListTile(
-          //   leading: const Icon(Icons.info),
-          //   title: const Text('About Us'),
-          //   trailing: const Icon(Icons.chevron_right),
-          //   onTap: () {
-          //     // Navigate to About Us Screen
-          //   },
-          // ),
-          // const Divider(),
-          // ListTile(
-          //   leading: const Icon(Icons.logout),
-          //   title: const Text('Logout'),
-          //   onTap: () {
-          //     // Handle Logout
-          //     showDialog(
-          //       context: context,
-          //       builder: (context) => AlertDialog(
-          //         title: const Text('Logout'),
-          //         content: const Text('Are you sure you want to logout?'),
-          //         actions: [
-          //           TextButton(
-          //             onPressed: () => Navigator.of(context).pop(),
-          //             child: const Text('Cancel'),
-          //           ),
-          //           TextButton(
-          //             onPressed: () {
-          //               //jika textbutton diklik otomatis apk tertutup
-          //               SystemNavigator.pop();
-
-          //               // Navigator.push(
-          //               //   context,
-          //               //   MaterialPageRoute(
-          //               //       builder: (context) => const SplashScreen()),
-          //               // );
-          //             },
-          //             child: const Text('Logout'),
-          //           ),
-          //         ],
-          //       ),
-          //     );
-          //   },
-          // ),
         ],
       ),
     );
